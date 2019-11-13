fb8531e0695098002f576bac2a31d4be3d90397e
Ivaylo Kostov <ivaylo.kostov@code-runners.com>
2019-11-13 18:20:52 +0200

    Added component with input for one stock price card

---
+++ Angular-Exercises/stocks-app/src/app/components/summary/summary.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'summary',
+  templateUrl: './summary.component.html',
+  styleUrls: ['./summary.component.scss']
+})
+export class SummaryComponent {
+
+  @Input() stock: any;
+
+  constructor() {
+    debugger;
+  }
+
+  isNegative() {
+    return (this.stock && this.stock.change < 0);
+  }
+
+  isPositive() {
+    return (this.stock && this.stock.change > 0);
+  }
+}
