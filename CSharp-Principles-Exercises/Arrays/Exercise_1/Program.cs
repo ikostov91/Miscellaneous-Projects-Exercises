8f9d39829d985cf0c3da79eef299818fdd64a66a
Ivaylo Kostov <ivaylo.kostov@code-runners.com>
2019-11-13 16:59:28 +0200

    Added exercises in CSharp arrays and exercise project for Angular

---
+++ CSharp-Principles-Exercises/Arrays/Exercise_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i * 5;
+            }
+
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
---
+++ CSharp-Principles-Exercises/Arrays/Exercise_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOne = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] arrayTwo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            if (arrayOne.Length != arrayTwo.Length)
+            {
+                Console.WriteLine("Not Equal!");
+                return;
+            }
+
+            for (int i = 0; i < arrayOne.Length; i++)
+            {
+                if (arrayOne[i] != arrayTwo[i])
+                {
+                    Console.WriteLine("Not Equal!");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Arrays are equal!");
+        }
+    }
+}
