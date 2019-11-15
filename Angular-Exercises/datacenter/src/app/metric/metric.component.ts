06e54a0a95ae89d62a06b6c40564f8ea99dcb13b
Ivaylo Kostov <ivaylo.kostov@code-runners.com>
2019-11-15 15:56:06 +0200

    Completed datacenter project

---
+++ Angular-Exercises/datacenter/src/app/metric/metric.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-metric',
+  templateUrl: './metric.component.html',
+  styleUrls: ['./metric.component.css']
+})
+export class MetricComponent {
+
+  @Input() title: string = '';
+  
+  private _value: number = 0;
+  private _max: number = 100;
+
+  @Input('used')
+  set value(value: number) {
+    if (isNaN(value)) {
+      value = 0;
+    }
+    this._value = value;
+  }
+
+  get value() : number {
+    return this._value;
+  }
+
+  @Input('available') 
+  set max(value: number) {
+    if (isNaN(value)) {
+      value = 100;
+    }
+    this._max = value;
+  }
+
+  get max() : number {
+    return this._max;
+  }
+
+  constructor() { }
+
+  isDanger() {
+    return this.value / this.max > 0.7;
+  }
+}
